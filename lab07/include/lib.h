1800cfe97aa893a3380e218204fcd061efe4e5ac
grishko4210g <grishko4210@gmail.com>
2025-11-30 18:03:25 +0000

    Remove lab07 folder

--- lab07/include/lib.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIB_H_
-#define LIB_H_
-
-double f11(double x, double y, double z, double w); // x + log(y+1) - √z * cos(w)
-double f15(double x, double y, double z, double w); // x - √y * (exp(z) + sin(w))
-double f20(double x, double y, double z, double w); // x + exp(y) * √z - sin(w)
-double f5(double x, double y, double z, double w, double v); // x * sin(y) / cos(z) + w -√v
-
-#endif // LIB_H_
