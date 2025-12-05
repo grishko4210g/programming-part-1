49787162a513487b1b259fe4e9355d3ac8916a11
grishko4210g <grishko4210@gmail.com>
2025-12-05 07:58:25 +0000

    Add Change for lab12 files

--- lab12/task1/task1_strspn.c
+++ lab12/task1/task1_strspn.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 
-size_t my_file(const char *str, const char *accept) {
-    const char *s = str;
-    const char *a;
+size_t my_file_copy(const char *str, const char *accept) {
+    const char *current = str;
+    const char *acc_ptr = accept;
 
     size_t count = 0;
 
-    while (*s) {
-        a = accept;
+    while (*current) {
+        acc_ptr = accept;
         int match = 0;
 
-        while (*a) {
-            if(*s == *a) {
+        while(*acc_ptr) {
+            if (*current == *acc_ptr) {
                 match = 1;
                 break;
             }
-            a++;
+            acc_ptr++;
         }
-        if(!match)
-        break;
+        if (!match) {
+            break;
+        }
         count++;
-        s++;
+        current++;
     }
     return count;
 }
@@ -30,7 +31,7 @@ int main () {
     const char *str = "exchange";
     const char *acc = "change782";
 
-    size_t b = my_file(str, acc);
-    printf("Results: %zu \n", b);
+    size_t result = my_file_copy(str, acc);
+    printf("Results: %zu \n", result);
     return 0;
 }
